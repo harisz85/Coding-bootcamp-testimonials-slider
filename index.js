c54fb64a86709d0e2e1682cd400b4e9567c42424
Haris Zikos <45425032+harisz85@users.noreply.github.com>
2021-06-20 13:46:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const reviews = [
     {
         id : 1 ,
         author : "Tanya Sinclair",
-        img : "/images/image-tanya.jpg",
+        img : "images/image-tanya.jpg",
         text: "“ I’ve been interested in coding for a while but never taken the jump, until now.  I couldn’t recommend this course enough.I’m now in the job of my dreams and so  excited about the future.”",
         job: "UX Engineer",
     },
     {
         id: 2,
         author: "John Tarkpor",
-        img : "/images/image-john.jpg",
+        img : "images/image-john.jpg",
         text : "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible.I now feel so confident about starting up as a professional developer.”",
         job : "Junior Front-end Developer",
     }
